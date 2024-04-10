2098ea001b8b04ed613878aa33f367fe9dc6e304
Elisberto Junior <elisberto_jr@hotmail.com>
2024-04-10 00:11:29 -0300

    adicionando conrollers

--- src/main/java/com/efood/controller/KitchenController.java
+++ src/main/java/com/efood/controller/KitchenController.java
@@ -2,12 +2,13 @@ package com.efood.controller;
 
 import com.efood.infrastructure.repository.KitchenRepositoryJpa;
 import com.efood.model.Kitchen;
+import com.fasterxml.jackson.databind.util.BeanUtil;
+import org.springframework.beans.BeanUtils;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,6 +26,53 @@ public class KitchenController {
     @GetMapping
     public ResponseEntity<List<Kitchen>> list() {
         return ResponseEntity.ok().body(repository.list());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Kitchen> search(@PathVariable Long id) {
+        Kitchen kitchen = repository.search(id);
+
+        if (kitchen != null)
+            return ResponseEntity.ok(kitchen);
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    }
+
+    @PostMapping
+    //@ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<Kitchen> add(@RequestBody Kitchen kitchen) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(kitchen));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Kitchen> update(@PathVariable Long id, @RequestBody Kitchen kitchen) {
+        Kitchen kitchenDB = repository.search(id);
+
+        if(kitchenDB != null) {
+            BeanUtils.copyProperties(kitchen, kitchenDB, "id");
+
+            repository.save(kitchenDB);
+
+            return ResponseEntity.ok(kitchenDB);
+        }
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Kitchen> remove(@PathVariable Long id) {
+        Kitchen kitchen = repository.search(id);
+        try {
+            if(kitchen != null) {
+                repository.remove(kitchen);
+                return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+            }
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+
+        } catch (DataIntegrityViolationException e) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).build();
+        }
+
     }
 
 }
--- src/main/java/com/efood/controller/StateController.java
+++ src/main/java/com/efood/controller/StateController.java
@@ -3,9 +3,7 @@ package com.efood.controller;
 import com.efood.infrastructure.repository.StateRepositoryJpa;
 import com.efood.model.State;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,4 +21,10 @@ public class StateController {
     public ResponseEntity<List<State>> list() {
         return ResponseEntity.ok().body(repository.list());
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<State> search(@PathVariable Long id) {
+        return ResponseEntity.ok().body(repository.search(id));
+    }
+
 }
